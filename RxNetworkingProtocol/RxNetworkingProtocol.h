2234cb5d85aaa0e5ba82ef3c7e25db7e4bffac38
Patryk Mikolajczyk <p.mikolajczyk@bsgroup.eu>
2019-01-29 09:51:55 +0100

    Initial Commit

---
+++ RxNetworkingProtocol/RxNetworkingProtocol.h
@@ -0,0 +1,19 @@
+//
+//  RxNetworkingProtocol.h
+//  RxNetworkingProtocol
+//
+//  Created by Patryk Mikolajczyk on 1/29/19.
+//  Copyright © 2019 Patryk Mikolajczyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNetworkingProtocol.
+FOUNDATION_EXPORT double RxNetworkingProtocolVersionNumber;
+
+//! Project version string for RxNetworkingProtocol.
+FOUNDATION_EXPORT const unsigned char RxNetworkingProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNetworkingProtocol/PublicHeader.h>
+
+
